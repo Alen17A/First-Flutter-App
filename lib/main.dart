518be99c020924c143e3b46e5b9bcaf6b0714ec9
Alen Jose <alenjose812@gmail.com>
2024-11-10 14:14:41 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "First Flutter App",
+    home: Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(""),
+        ),
+      ),
+    )
+  ))
+}
